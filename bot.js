f3b3410ced11f81dabd259bb4f661af6f27d015c
DreamProgramServer <31097959+DreamProgramServer@users.noreply.github.com>
2018-11-24 17:05:18 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,24 @@
 var Discord = require('discord.js');
 var bot = new Discord.Client();
+const fs = require('fs');
 
 bot.on('message', message => {
   var sender = message.author;
   var msg = message.content.toUpperCase();
   var prefix = '>'
 
-  //command
-if (msg === prefix + 'CC') {
-      message.channel.send( 'The Bot has connected!' )
-    }
-
+  //Basic---Command
+  if (msg === prefix + 'CC') {
+    message.channel.send( 'The Bot has connected!' )
+  }
   
-  
-  
+  //Eco System
+  if (!userdata[sender.id + message.guild.id])  userdata[sender.id + message.guild.id] = {}
+  if (!userdata[sender.id + message.guild.id].money) userdata[sender.id + message.guild.id].money = 100;
+  //Eco System---Command
+  if (msg === prefix + 'Money') {
+    message.channel.send(userdata[sender.id + message.guild.id].money)
+  }
 });
 
 //Event
